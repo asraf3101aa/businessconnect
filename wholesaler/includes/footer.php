42318944725abb373900ecfe6675f2b0c6439e26
Asraf Ansari <asraf3101aa@gmail.com>
2022-06-14 09:51:39 +0545

    chganges

--- wholesaler/includes/footer.php
+++ wholesaler/includes/footer.php
@@ -73,8 +73,8 @@
             <div class="footer-nav__col footer-nav__col--contacts">
                 <div class="footer-nav__heading">Contact details</div>
                 <address class="address">
-                    Makawanpur, Nepal.<br>
-                    Hetauda, 44107.
+                Head Office: Melbourne CBD.<br>
+                    Melbourne, Victoria, CBD 3000.
                 </address>
                 <div class="phone">
                     Telephone:
